65c11bcc44ea0523016aef66f21687c07e732573
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-11-16 17:43:23 +0100

    modified grid for project section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -3,14 +3,14 @@ import ProjectCard from './ProjectCard'
 
 const projectsData = [
     {
-        id:1,
+        id: 1,
         title: 'Weather App',
         description: 'Weather App built in react',
         image: '/images/projects/weather-app.png',
         tag: ['React']
     },
     {
-        id:2,
+        id: 2,
         title: 'Calculator',
         description: 'Calculator App built in react',
         image: '/images/projects/calculator.png',
@@ -19,16 +19,23 @@ const projectsData = [
 ]
 
 const ProjectsSection = () => {
-  return (
-    <>
-        <h2>My Projects</h2>
-        <div>
-            {projectsData.map((proj)=>(
-                <ProjectCard key={proj.id} title={proj.title} description={proj.description} imgUrl={proj.image} />
-            ))}
-        </div>
-    </>
-)
+    return (
+        <>
+            <h2>My Projects</h2>
+            <div className='md:grid md:grid-cols-2 gap-8'>
+                {projectsData.map((proj) => (
+                    <div key={proj.id} className='cols-12'>
+                        <ProjectCard  title={proj.title} description={proj.description} imgUrl={proj.image} />
+                     </div>
+                ))}
+                {projectsData.map((proj) => (
+                     <div key={proj.id} className='cols-12'>
+                        <ProjectCard  title={proj.title} description={proj.description} imgUrl={proj.image} />
+                     </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default ProjectsSection
