a0289c21989689421d017ff46d4078727172e2d9
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-11-17 17:10:56 +0100

    finished projects section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -4,10 +4,6 @@ import ProjectCard from './ProjectCard'
 import ProjectTag from './ProjectTag';
 import { useState } from 'react';
 
-const tags = [
-
-];
-
 const projectsData = [
     {
         id: 1,
@@ -16,7 +12,7 @@ const projectsData = [
         image: '/images/projects/weather-app.png',
         git: 'https://github.com/AnnalisaCaldarulo97/react_weather_app',
         preview: '',
-        tag: ['All', 'React']
+        tag: ['All', 'Frontend']
     },
     {
         id: 2,
@@ -25,44 +21,64 @@ const projectsData = [
         image: '/images/projects/calculator.png',
         git: 'https://github.com/AnnalisaCaldarulo97/calculator_react',
         preview: '',
-        tag: ['All', 'React']
+        tag: ['All', 'Frontend']
+    },
+    {
+        id: 3,
+        title: 'Weather App',
+        description: 'Weather App built in react',
+        image: '/images/projects/weather-app.png',
+        git: 'https://github.com/AnnalisaCaldarulo97/react_weather_app',
+        preview: '',
+        tag: ['All', 'Backend']
+    },
+    {
+        id: 4,
+        title: 'Calculator',
+        description: 'Calculator App built in react',
+        image: '/images/projects/calculator.png',
+        git: 'https://github.com/AnnalisaCaldarulo97/calculator_react',
+        preview: '',
+        tag: ['All', 'Backend']
     },
 ]
 
 const ProjectsSection = () => {
     const [tag, setTag] = useState('All');
-    const handleTagCHange = (newTag) => {
-        setTag(newTag);
-    }
+    const handleTagCHange = (newTag) => (
+        setTag(newTag)
+    );
+
+    // funzione di filtraggio tra i projects
+    const filteredProjects = projectsData.filter((proj) => {
+        return proj.tag.includes(tag);
+    });
+
     return (
         <div id='projects'>
             <h2>My Projects</h2>
             {/* filter buttons */}
             <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
-                {/* <button className='rounded-full border-2 border-purple-500 px-6 py-6 text-lg cursor-pointer'>Frontend</button>
-                <button className='rounded-full border-2 border-slate-600 px-6 py-6 text-lg md:text-xl cursor-pointer hover:border-white'>Backend</button> */}
                 <ProjectTag
                     onClick={handleTagCHange}
-                    tag='All'
                     name='All'
                     isSelected={tag == 'All'}
                 />
                 <ProjectTag
                     onClick={handleTagCHange}
-                    tag='React'
-                    name='React'
-                    isSelected={tag == 'React'}
+                    name='Frontend'
+                    isSelected={tag == 'Frontend'}
+                />
+                 <ProjectTag
+                    onClick={handleTagCHange}
+                    name='Backend'
+                    isSelected={tag == 'Backend'}
                 />
             </div>
 
             {/* cards */}
-            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-                {projectsData.map((proj) => (
-                    <div key={proj.id} className='cols-12'>
-                        <ProjectCard previewUrl={proj.preview} gitUrl={proj.git} title={proj.title} description={proj.description} imgUrl={proj.image} />
-                    </div>
-                ))}
-                {projectsData.map((proj) => (
+            <div className='grid md:grid-cols-2 gap-8 md:gap-12 '>
+                {filteredProjects.map((proj) => (
                     <div key={proj.id} className='cols-12'>
                         <ProjectCard previewUrl={proj.preview} gitUrl={proj.git} title={proj.title} description={proj.description} imgUrl={proj.image} />
                     </div>
